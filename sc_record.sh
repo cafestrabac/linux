26e71cd5b747611ef60ee70365467b4e4c37dfbf
quoije <titmatt94@gmail.com>
2018-02-26 01:21:24 -0500

    fix locations for html5 player

--- sc_record.sh
+++ sc_record.sh
@@ -3,7 +3,7 @@
 
 DATE="`date '+%Y_%m_%d_%I_%M_%S_%p'`"
 
-if [ -f /home/cafe/shoutcast/rec/oblc.lock ] && kill -0 $(cat /home/cafe/shoutcast/rec/oblc.lock);
+if [ -f /home/oblc/sc_oblc.lock ] && kill -0 $(cat /home/oblc/sc_oblc.lock);
 then
   echo "Already running. Exiting."
   exit 1
@@ -11,11 +11,11 @@ else
 if
 curl -Is https://oblc.ca:8002/\;stream.mp3 | grep "200 OK" > /dev/null
 then
-        echo $$ > /home/cafe/shoutcast/rec/oblc.lock
-        ffmpeg -i https://oblc.ca:8002/\;stream.mp3 /home/cafe/shoutcast/rec/OBLC_$DATE.mp3
-        rm /home/cafe/shoutcast/rec/oblc.lock
+  echo $$ > /home/oblc/sc_oblc.lock
+  ffmpeg -i https://oblc.ca:8002/\;stream.mp3 /home/cafe/shoutcast/rec/OBLC_$DATE.mp3
+  rm /home/oblc/sc_oblc.lock
 else
-        echo "Stream is not up!"
-        exit 1
+  echo "Stream is not up!"
+  exit 1
 fi
 fi
