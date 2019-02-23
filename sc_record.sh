2e8498c1d53eb4e3af8111b2b7767df039a549bf
quoije <titmatt94@gmail.com>
2019-02-23 03:15:16 -0500

    Add FB live with still image

--- sc_record.sh
+++ sc_record.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Shell script to automatically record record my shoutcast stream
+# Shell script to automatically record my shoutcast stream in mp3 and stream it to FB with a still image.
 
 DATE="`date '+%Y_%m_%d_%I_%M_%S_%p'`"
 
@@ -9,13 +9,17 @@ then
   exit 1
 else
 if
-curl -Is https://oblc.ca:8002/\;stream.mp3 | grep "200 OK" > /dev/null
+curl -Is http://website:8000/\;stream.mp3 | grep "200 OK" > /dev/null
 then
   echo $$ > /home/cafe/shoutcast/rec/oblc.lock
-  ffmpeg -i https://oblc.ca:8002/\;stream.mp3 /home/cafe/shoutcast/rec/OBLC_$DATE.mp3
+  ffmpeg -r 30 -loop 1 -i /home/cafe/shoutcast/video.png \
+  -i http://website:8000/\;stream.mp3 /home/cafe/shoutcast/rec/OBLC_$DATE.mp3 \
+  -c:a aac -c:v h264 -b:v 768k \
+  -preset ultrafast -tune stillimage -pix_fmt yuvj444p -g 60 \
+  -profile:v high444 -level 4.2 \
+  -f flv "rtmp://live-api-s.facebook.com:80/rtmp/{FBKEY}"
   rm /home/cafe/shoutcast/rec/oblc.lock
 else
   echo "Stream is not up!"
   exit 1
-fi
 fi
